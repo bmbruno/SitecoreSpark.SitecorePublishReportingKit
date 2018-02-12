0b4fe0dc49c2737d98c1374eb3b595f265c16335
Brandon Bruno <bmbruno@gmail.com>
2018-02-12 09:16:35 -0500

    Added LogIndex/LogItem viewmodels.

---
+++ SitecoreSpark.SitecorePublishReportingKit/ViewModels/LogIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreSpark.SPRK.ViewModels
+{
+    public class LogIndexViewModel
+    {
+        List<LogItemViewModel> LogItems { get; set; }
+
+        public LogIndexViewModel()
+        {
+            LogItems = new List<LogItemViewModel>();
+        }
+    }
+}
---
+++ SitecoreSpark.SitecorePublishReportingKit/ViewModels/LogItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreSpark.SPRK.ViewModels
+{
+    public class LogItemViewModel
+    {
+        public string Date { get; set; }
+
+        public string SizeKB { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
