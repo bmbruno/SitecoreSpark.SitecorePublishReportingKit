88efe94c5c9a3efc93b536cb2c19db27790c6d5b
Brandon Bruno <bmbruno@gmail.com>
2019-11-03 08:39:52 -0500

    Feature/debug info (#11)
    * Added Debug features.
    
    * Added check to DebugMode setting before allowing debug interface to load. Removed extra cruft from gitignore.
    
    * Updated package definition file with debug changes.

---
+++ SitecoreSpark.SitecorePublishReportingKit/ViewModels/DebugViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreSpark.SPRK.ViewModels
+{
+    public class DebugViewModel
+    {
+        public DebugViewModel() { }
+
+        public bool Enabled;
+        public bool DebugMode;
+        public string LogFolder;
+        public string LogPrefix;
+        public int MaxLogBufferSize;
+        public string SourceDatabase;
+        public string TargetDatabase;
+    }
+}
