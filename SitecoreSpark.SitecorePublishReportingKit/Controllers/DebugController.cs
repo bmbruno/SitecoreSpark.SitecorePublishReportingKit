b6f4c5734b05a504c58a33477b7cc913a9958d7a
Brandon Bruno <bmbruno@gmail.com>
2020-04-03 09:50:35 -0400

    Updated NuGet references for Sitecore.Kernel to use IgnoreDependencies.

--- SitecoreSpark.SitecorePublishReportingKit/Controllers/DebugController.cs
+++ SitecoreSpark.SitecorePublishReportingKit/Controllers/DebugController.cs
@@ -3,7 +3,6 @@ using System.Web.Mvc;
 using System.Text;
 using System.Collections.Generic;
 using Sitecore.Data;
-
 using SitecoreSpark.SPRK.Interfaces;
 using SitecoreSpark.SPRK.Models;
 using SitecoreSpark.SPRK.ViewModels;
